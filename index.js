4ba558eda57115bfca76ebaa2e2e76544214646e
SpaceCooler94 <astronuatkid@gmail.com>
2026-04-04 11:44:31 -0400

    Implement NBA stats fetching in server response
    Added a function to fetch NBA stats from an API and updated the server response to return the fetched data.

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const http = require('http');
+const https = require('https');
 
-const server = http.createServer((req, res) => {
+function fetchNBAStats(url) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      headers: {
+        'User-Agent': 'Mozilla/5.0',
+        'Referer': 'https://www.nba.com/',
+        'Origin': 'https://www.nba.com',
+        'Accept': 'application/json'
+      }
+    };
+    https.get(url, options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(JSON.parse(data)));
+    }).on('error', reject);
+  });
+}
+
+const server = http.createServer(async (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify({ status: 'ok', message: 'prop-backend is alive' }));
+  
+  const url = 'https://stats.nba.com/stats/leaguedashplayerstats?Season=2024-25&SeasonType=Regular+Season&PerMode=PerGame';
+  const data = await fetchNBAStats(url);
+  res.end(JSON.stringify(data));
 });
 
 server.listen(process.env.PORT || 3000, () => {
