d3d84f8f896f7c614047d8ff61c566fa8f13d4c5
SpaceCooler94 <astronuatkid@gmail.com>
2026-04-04 11:33:23 -0400

    Create a basic HTTP server in Index.js

---
+++ Index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify({ status: 'ok', message: 'prop-backend is alive' }));
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
+});
