d595f55ed1683218b639af02c479a75174779b3f
SpaceCooler94 <astronuatkid@gmail.com>
2026-04-04 12:21:14 -0400

    Refactor NBA stats fetching to use Tank01 API

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const http = require('http');
 const https = require('https');
 
-function fetchNBAStats(url) {
+const RAPIDAPI_KEY = process.env.RAPIDAPI_KEY;
+
+function fetchTank01(endpoint, params) {
   return new Promise((resolve, reject) => {
+    const queryString = new URLSearchParams(params).toString();
+    const url = `https://tank01-fantasy-stats.p.rapidapi.com/${endpoint}?${queryString}`;
+    
     const options = {
       headers: {
-        'User-Agent': 'Mozilla/5.0',
-        'Referer': 'https://www.nba.com/',
-        'Origin': 'https://www.nba.com',
-        'Accept': 'application/json'
-      },
-      timeout: 10000
+        'x-rapidapi-host': 'tank01-fantasy-stats.p.rapidapi.com',
+        'x-rapidapi-key': RAPIDAPI_KEY
+      }
     };
-    const req = https.get(url, options, (res) => {
+
+    https.get(url, options, (res) => {
       let data = '';
       res.on('data', chunk => data += chunk);
       res.on('end', () => {
@@ -22,18 +25,19 @@ function fetchNBAStats(url) {
           reject(new Error('Failed to parse response'));
         }
       });
-    });
-    req.on('timeout', () => reject(new Error('Request timed out')));
-    req.on('error', reject);
+    }).on('error', reject);
   });
 }
 
 const server = http.createServer(async (req, res) => {
   res.writeHead(200, {'Content-Type': 'application/json'});
   try {
-    const url = 'https://stats.nba.com/stats/leaguedashplayerstats?Season=2024-25&SeasonType=Regular+Season&PerMode=PerGame';
-    const data = await fetchNBAStats(url);
-    res.end(JSON.stringify({ status: 'ok', rowCount: data.resultSets[0].rowSet.length }));
+    const data = await fetchTank01('getNBAGamesForPlayer', {
+      playerID: '28268405032',
+      numberOfGames: '10',
+      season: '2024'
+    });
+    res.end(JSON.stringify(data));
   } catch(e) {
     res.end(JSON.stringify({ status: 'error', message: e.message }));
   }
